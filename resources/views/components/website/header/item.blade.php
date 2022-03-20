06755190806d837fee0255035f18d567b8f69f01
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-20 22:54:47 +0300

    quick fix

--- resources/views/components/website/header/item.blade.php
+++ resources/views/components/website/header/item.blade.php
@@ -1,4 +1,4 @@
 <div class="hover:border-b-2 hover:border-red-600 {{ ($show??false) == false ? 'hidden md:block' : '' }}">
     <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-    <a href="{{ $href }}">{{ $title }}</a>
+    <a href="{{ $href??'#' }}">{{ $title??'Title' }}</a>
 </div>
