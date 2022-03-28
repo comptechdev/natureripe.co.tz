e06383830eb0c935971518ad051a600a0132fc60
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-28 11:18:56 +0300

    fixed the recepies component unknown variable error

--- resources/views/components/website/about/recepies.blade.php
+++ resources/views/components/website/about/recepies.blade.php
@@ -1,7 +1,7 @@
 <div class="row-span-3" style="background-image: url({{ $background ?? asset('img/bg1.png') }}) ">
     <div class="flex justify-center py-5">
         <div class="z-10 hover:rotate-12 duration-1000 transition">
-            <img src=" {{ $bottle }}" alt="bt" class="h-64">
+            <img src=" {{ $bottle??'#' }}" alt="bt" class="h-64">
         </div> 
         <div class="w-64 bg-white p-10 -ml-10">
             <div class="">
