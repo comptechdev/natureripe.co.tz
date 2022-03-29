58d5e88f3846fe05e7b7ac9ff9fae8b302b4b516
Mark - DopestGeek <markmayalla@gmail.com>
2022-03-29 10:58:55 +0300

    created initial models and migrations

---
+++ database/migrations/2022_03_29_060814_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid("product_id");
+            $table->string("name");
+            $table->string("featured_image")->nullable();
+            $table->text("description")->nullable();
+            $table->string("nutrition_unit")->nullable();
+            $table->string("nutrition_calories")->nullable();
+            $table->string("nutrition_total_fat")->nullable();
+            $table->string("nutrition_saturated_fat")->nullable();
+            $table->string("nutrition_trans_fat")->nullable();
+            $table->string("nutrition_cholesterol")->nullable();
+            $table->string("nutrition_sodium")->nullable();
+            $table->string("nutrition_carbohydrates")->nullable();
+            $table->string("nutrition_dietary_fiber")->nullable();
+            $table->string("nutrition_sugers")->nullable();
+            $table->string("nutrition_protein")->nullable();
+            $table->string("nutrition_vitamin_c")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2022_03_29_063738_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->uuid("collection_id");
+            $table->string("name");
+            $table->string("featured_image")->nullable();
+            $table->text("description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+};
