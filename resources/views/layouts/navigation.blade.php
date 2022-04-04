977e381e48dc24c84e3aa54b3bc208d2f46ce1a6
Mark - DopestGeek <markmayalla@gmail.com>
2022-04-04 21:20:50 +0300

    Fixed the menu bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('resources.products.index')" :active="request()->routeIs('products')">
+                    <x-nav-link :href="route('resources.products.index')" :active="request()->routeIs('resources.products.*')">
                         {{ __('Products') }}
                     </x-nav-link>
                 </div>
@@ -83,6 +83,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('resources.products.index')" :active="request()->routeIs('resources.products.*')">
+                {{ __('Products') }}
             </x-responsive-nav-link>
         </div>
 
