efb0e301edea6e73a195fb203c26b78175587514
Mark - DopestGeek <markmayalla@gmail.com>
2022-05-19 16:06:49 +0300

    fixed null error a na component

--- resources/views/components/website/body/section.blade.php
+++ resources/views/components/website/body/section.blade.php
@@ -2,16 +2,18 @@
     <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
 
     <div class="flex flex-nowrap gap-2 overflow-x-scroll scrollbar-hide">
-        @foreach ($meals as $meal)
-        <div class="flex-none rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="{{ route("website.meal", ["meal" => $meal]) }}">
-                <img src="{{ $meal->featured_image_url }}" alt="tb1" class="rounded-1-lg">
-            </a>
-            <div class="pt-2">
-                <button type="button" class="inline-block w-full  px-6 py-6 bg-slate-700 text-white font-bold text-xl leading-tight uppercase shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-600 active:shadow-lg transition duration-150 ease-in-out">{{ $meal->name }}</button>
-            </div>
-        </div>
-        @endforeach
+        @isset($meals)
+            @foreach ($meals as $meal)
+                <div class="flex-none rounded-lg shadow-lg bg-white max-w-sm">
+                    <a href="{{ route("website.meal", ["meal" => $meal]) }}">
+                        <img src="{{ $meal->featured_image_url }}" alt="tb1" class="rounded-1-lg">
+                    </a>
+                    <div class="pt-2">
+                        <button type="button" class="inline-block w-full  px-6 py-6 bg-slate-700 text-white font-bold text-xl leading-tight uppercase shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-600 active:shadow-lg transition duration-150 ease-in-out">{{ $meal->name }}</button>
+                    </div>
+                </div>
+            @endforeach
+        @endisset
         
         {{-- <div class="flex-none rounded-lg shadow-lg bg-white max-w-sm">
             <a href="#">
