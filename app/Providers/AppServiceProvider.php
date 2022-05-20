001358e515a6c6c52daf1a6627074aed0c574c70
Mark - DopestGeek <markmayalla@gmail.com>
2022-05-20 12:03:03 +0300

    fixed some components that cause some problems

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\View\Components\UI\Buttons\Elevated;
 use App\View\Components\UI\Core\Button;
 use App\View\Components\UI\Core\Input;
+use App\View\Components\website\body\section;
+use App\View\Components\website\body\slider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -22,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
         Blade::component("ui.core.button", Button::class);
         Blade::component("ui.buttons.elevated", Elevated::class);
         Blade::component("ui.core.input", Input::class);
+        Blade::component("website.body.slider", slider::class);
+        Blade::component("website.body.section", section::class);
     }
 
     /**
