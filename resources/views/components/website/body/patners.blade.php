d5342367b7c476a241dddfa8d5dd563f5cb70552
Derick Munisi <derickmunisi@gmail.com>
2022-08-01 10:53:54 +0300

    update

--- resources/views/components/website/body/patners.blade.php
+++ resources/views/components/website/body/patners.blade.php
@@ -2,8 +2,9 @@
     <h1 class="font-bold text-2xl md:text-4xl text-center pt-5 md:pt-7 pb-2 md:pb-5 md:py-14">Our Patners</h1>
     <div class="flex justify-center gap-10 md:gap-32 py-10">   
     <div><img src="{{ asset('img/p22.png') }}" alt=""></div>
-    <div><img src="{{ asset('img/p11.png') }}" alt=""></div>
+    <div><img src="{{ asset('img/p221.png') }}" alt=""></div>
     <div><img src="{{ asset('img/5.png') }}" alt=""></div>
+    <div><img src="{{ asset('img/p222.png') }}" alt=""></div>
     <div><img src="{{ asset('img/p44.png') }}" alt=""></div>
     </div>
 </div>
